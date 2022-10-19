aff6faa075e7cc63b1159a93b5800468b9b18849
LucyMenges <llanzonimenges@gmail.com>
2022-10-18 22:03:55 -0300

    incio

--- Script Projeto Setor Publico.sql
+++ Script Projeto Setor Publico.sql
@@ -1,5 +1,16 @@
 # PROJETO 2 - RESIDÊNCIA TECH AD 
-# PROJETO ANÁLISE DE DADOS SETOR´PÚBLICO.
+# PROJETO ANÁLISE DE DADOS SETOR PÚBLICO.
 
-use residenciatechad;
+--conectando ao banco de dados
+USE residenciatechad;
+
+--Verificando o tipo de colunas no arquivo aditivos
 describe dados_abertos_aditivos;
+
+--Alterando o tipo da coluna data_inicio_vigencia e data_fim_vigencia para date
+
+ALTER TABLE dados_abertos_aditivos
+ADD PRIMARY KEY(num_contrato);
+
+
+
